ed91ecaf596479b738a27ac56b6c8a454ab52131
Follpvosten <wolfi@karpador.xyz>
2019-08-25 09:37:27 +0100

    Remodeled API again

--- src/lib.rs
+++ src/lib.rs
@@ -9,7 +9,6 @@
 //! Create a pipe, write to it in one async task and read from it in another:
 //!
 //! ```
-//! # #![feature(async_await)]
 //! # fn main() -> async_std::io::Result<()> { async_std::task::block_on(async {
 //! #
 //! use unix_fifo_async::NamedPipePath;
@@ -18,21 +17,26 @@
 //! // Create a new pipe at the given path
 //! let pipe = NamedPipePath::new("./my_pipe");
 //! // This creates the path if it doesn't exist; it may return a nix::Error
+//! // You can also use the `ensure_pipe_exists` convenience function on
+//! // readers/writers, but calling it on both at the same time results
+//! // in a race condition so it can never succeed.
 //! pipe.ensure_exists().unwrap();
 //! // Create a writer and a reader on the path
-//! let writer = pipe.open_write().await?;
-//! let reader = pipe.open_read().await?;
+//! let writer = pipe.open_write();
+//! let reader = pipe.open_read();
 //!
 //! // Some data we can send over the pipe
 //! let data_to_send = "Hello, pipes!";
 //!
 //! // Spawn two tasks, one for writing to and one for reading from the pipe.
+//! // Note that in practice, you'll probably want to read the pipe from a
+//! // different process or a different program entirely.
 //! let t1 = task::spawn(async move { writer.write_str(data_to_send).await });
 //! let t2 = task::spawn(async move { reader.read_string().await });
 //!
 //! // `.await` both tasks and compare the result with the original
-//! t1.await.unwrap();
-//! let read_result = t2.await.unwrap();
+//! t1.await?;
+//! let read_result = t2.await?;
 //! assert_eq!(read_result, data_to_send);
 //!
 //! // Delete the pipe
@@ -40,7 +44,6 @@
 //! # Ok(())
 //! # })}
 //! ```
-#![feature(async_await)]
 
 mod named_pipe;
 
--- src/named_pipe.rs
+++ src/named_pipe.rs
@@ -1,164 +1,118 @@
-use async_std::{
-    fs::File,
-    io::{self, Read, Write},
-};
+use async_std::{fs, io};
 use std::path::PathBuf;
 
 /// Represents a path to a Unix named pipe (FIFO).
 ///
 /// Provides convenience methods to create readers and writers, as well as an
 /// easy way to ensure the pipe actually exists.
+#[derive(Clone)]
 pub struct NamedPipePath {
-    path: PathBuf,
+    inner: PathBuf,
 }
 
 impl NamedPipePath {
     /// Wraps a given path in a `NamedPipePath`.
     pub fn new<T: Into<PathBuf>>(path: T) -> Self {
-        Self { path: path.into() }
+        Self { inner: path.into() }
     }
     /// Checks if the path exists.
     pub fn exists(&self) -> bool {
-        self.path.exists()
+        self.inner.exists()
     }
-    // Ensures the path exists, creating a named pipe in its place if it doesn't.
+    /// Ensures the path exists, creating a named pipe in its place if it doesn't.
     pub fn ensure_exists(&self) -> nix::Result<()> {
         if !self.exists() {
-            crate::create_pipe(&self.path, None)
+            crate::create_pipe(&self.inner, None)
         } else {
             Ok(())
         }
     }
     /// Tries to delete the pipe from disk and consumes the `NamedPipe`.
     pub async fn delete(self) -> io::Result<()> {
-        if self.path.exists() {
-            crate::remove_pipe(&self.path).await
+        if self.inner.exists() {
+            crate::remove_pipe(&self.inner).await
         } else {
             Ok(())
         }
     }
 
     /// Creates a reader for this named pipe.
-    pub async fn open_read(&self) -> io::Result<NamedPipeReader> {
-        NamedPipeReader::from_path(self).await
+    pub fn open_read(&self) -> NamedPipeReader {
+        NamedPipeReader::from_path(self)
     }
     /// Creates a writer for this named pipe.
-    pub async fn open_write(&self) -> io::Result<NamedPipeWriter> {
-        NamedPipeWriter::from_path(self).await
+    pub fn open_write(&self) -> NamedPipeWriter {
+        NamedPipeWriter::from_path(self)
     }
 }
 
 /// A convenience wrapper for reading from Unix named pipes.
-///
-/// Note: The actual idea of this struct is that it holds a File so it's
-/// not possible to just delete the pipe it's reading from. However, at the
-/// moment it doesn't seem to be possible to do that, as the `open`-call for
-/// that file will block until something is written to the pipe, kind of
-/// defeating the point of this wrapper. I'm currently considering workarounds,
-/// like checking whether the pipe exists and recreating iton each call to a
-/// read method.
 pub struct NamedPipeReader {
-    path: PathBuf,
+    path: NamedPipePath,
 }
 
 impl NamedPipeReader {
-    pub async fn from_path(source: &NamedPipePath) -> io::Result<Self> {
-        let path = source.path.clone();
-        //let handle = File::open(&path).await?;
-        Ok(Self { path })
+    /// Creates a new reader, cloning the given NamedPipePath.
+    pub fn from_path(source: &NamedPipePath) -> Self {
+        Self {
+            path: source.clone(),
+        }
+    }
+    /// Checks if the named pipe actually exists and tries to create it if it doesn't.
+    pub fn ensure_pipe_exists(&self) -> nix::Result<&Self> {
+        self.path.ensure_exists()?;
+        Ok(self)
     }
     /// Reads all bytes from the pipe.
-    /// This method is supposed to consume the reader in the future.
+    /// The returned Future will resolve when something is written to the pipe.
     pub async fn read(&self) -> io::Result<Vec<u8>> {
-        let mut handle = File::open(&self.path).await?;
-        let mut buf = Vec::new();
-        handle.read_to_end(&mut buf).await?;
-        Ok(buf)
+        fs::read(&self.path.inner).await
     }
-    // pub async fn read_reopen(&mut self) -> io::Result<Vec<u8>> {
-    //     let mut buf = Vec::new();
-    //     self.handle.read_to_end(&mut buf).await?;
-    //     self.handle = File::open(&self.path).await?;
-    //     Ok(buf)
-    // }
     /// Reads a String from the pipe.
-    /// This method is supposed to consume the reader in the future.
+    /// The returned Future will resolve when something is written to the pipe.
     pub async fn read_string(&self) -> io::Result<String> {
-        let mut handle = File::open(&self.path).await?;
-        let mut buf = String::new();
-        handle.read_to_string(&mut buf).await?;
-        Ok(buf)
+        fs::read_to_string(&self.path.inner).await
     }
-    // pub async fn read_string_reopen(&mut self) -> io::Result<String> {
-    //     let mut buf = String::new();
-    //     self.handle.read_to_string(&mut buf).await?;
-    //     self.handle = File::open(&self.path).await?;
-    //     Ok(buf)
-    // }
 }
 
 /// A convenience wrapper for writing to Unix named pipes.
-///
-/// Note: The actual idea of this struct is that it holds a File so it's
-/// not possible to just delete the pipe it's writing to. However, at the
-/// moment it doesn't seem to be possible to do that, as the `open`-call for
-/// that file will block until something is written to the pipe, kind of
-/// defeating the point of this wrapper. I'm currently considering workarounds,
-/// like checking whether the pipe exists and recreating it on each call to a
-/// write method.
 pub struct NamedPipeWriter {
-    path: PathBuf,
-    //handle: File,
+    path: NamedPipePath,
 }
 
 impl NamedPipeWriter {
-    async fn open(path: &PathBuf) -> io::Result<File> {
-        //use async_std::os::unix::fs::OpenOptionsExt;
-        //use nix::fcntl::OFlag;
-        async_std::fs::OpenOptions::new()
-            .write(true)
-            .create(false)
-            //.custom_flags(OFlag::O_NONBLOCK.bits())
-            .open(path)
-            .await
+    pub fn from_path(source: &NamedPipePath) -> Self {
+        Self {
+            path: source.clone(),
+        }
     }
-    pub async fn from_path(source: &NamedPipePath) -> io::Result<Self> {
-        let path = source.path.clone();
-        //let handle = Self::open(&path).await?;
-        Ok(Self { path })
+    /// Checks if the named pipe actually exists and tries to create it if it doesn't.
+    pub fn ensure_pipe_exists(&self) -> nix::Result<&Self> {
+        self.path.ensure_exists()?;
+        Ok(self)
     }
     /// Writes byte data to the pipe.
-    /// This method is supposed to consume the Writer in the future.
+    /// The returned Future will resolve when the bytes are read from the pipe.
     pub async fn write(&self, data: &[u8]) -> io::Result<()> {
-        let mut handle = Self::open(&self.path).await?;
-        handle.write_all(data).await
+        fs::write(&self.path.inner, data).await
     }
-    // pub async fn write_reopen(&mut self, data: &[u8]) -> io::Result<()> {
-    //     let mut handle = Self::open(&self.path).await?;
-    //     handle.write_all(data).await
-    // }
     /// Writes &str data to the pipe.
-    /// This method is supposed to consume the Writer in the future.
+    /// The returned Future will resolve when the string is read from the pipe.
     pub async fn write_str(&self, data: &str) -> io::Result<()> {
-        let mut handle = Self::open(&self.path).await?;
-        handle.write_all(data.as_bytes()).await
+        fs::write(&self.path.inner, data).await
     }
-    // pub async fn write_str_reopen(&mut self, data: &str) -> io::Result<()> {
-    //     let mut handle = Self::open(&self.path).await?;
-    //     handle.write_all(data.as_bytes()).await
-    // }
 }
 
 #[cfg(test)]
 mod tests {
-    use async_std::task::block_on;
+    use async_std::task::{self, block_on};
     #[test]
-    fn send_and_receive_threaded() {
+    fn write_and_read_threaded() {
         use std::thread;
         let pipe = super::NamedPipePath::new("./test_pipe_3");
         pipe.ensure_exists().unwrap();
-        let writer = block_on(pipe.open_write()).unwrap();
-        let reader = block_on(pipe.open_read()).unwrap();
+        let writer = pipe.open_write();
+        let reader = pipe.open_read();
         let data_to_send = "Hello pipe";
         let t_write = thread::spawn(move || block_on(writer.write_str(data_to_send)));
         let t_read = thread::spawn(move || block_on(reader.read_string()));
@@ -168,16 +122,54 @@ mod tests {
         block_on(pipe.delete()).unwrap();
     }
     #[test]
-    fn send_and_receive_async() {
+    fn write_and_read_async() {
         block_on(async {
-            use async_std::task;
             let pipe = super::NamedPipePath::new("./test_pipe_4");
             pipe.ensure_exists().unwrap();
-            let writer = pipe.open_write().await.unwrap();
-            let reader = pipe.open_read().await.unwrap();
+            let writer = pipe.open_write();
+            let reader = pipe.open_read();
             let data_to_send = "Hello pipe";
             let t1 = task::spawn(async move { writer.write_str(data_to_send).await });
             let t2 = task::spawn(async move { reader.read_string().await });
+            t1.await.unwrap();
+            let read_result = t2.await.unwrap();
+            assert_eq!(read_result, data_to_send);
+            pipe.delete().await.unwrap();
+        });
+    }
+    #[test]
+    fn ensure_on_write() {
+        block_on(async {
+            let pipe = super::NamedPipePath::new("./test_pipe_5");
+            let writer = pipe.open_write();
+            let reader = pipe.open_read();
+            let data_to_send = "Hello pipe";
+            let t1 = task::spawn(async move {
+                writer
+                    .ensure_pipe_exists()
+                    .unwrap()
+                    .write_str(data_to_send)
+                    .await
+            });
+            let t2 = task::spawn(async move { reader.read_string().await });
+            t1.await.unwrap();
+            let read_result = t2.await.unwrap();
+            assert_eq!(read_result, data_to_send);
+            pipe.delete().await.unwrap();
+        });
+    }
+    #[test]
+    fn ensure_on_read() {
+        block_on(async {
+            let pipe = super::NamedPipePath::new("./test_pipe_6");
+            let writer = pipe.open_write();
+            let reader = pipe.open_read();
+            let data_to_send = "Hello pipe";
+            let t1 = task::spawn(async move { writer.write_str(data_to_send).await });
+            let t2 =
+                task::spawn(
+                    async move { reader.ensure_pipe_exists().unwrap().read_string().await },
+                );
             t1.await.unwrap();
             let read_result = t2.await.unwrap();
             assert_eq!(read_result, data_to_send);
